7e2cb879adb010069300f6390c6d95f6254a4a82
Artsiom-Zhuk <artsiom.zhuk498@gmail.com>
2018-09-29 23:18:14 +0300

    add simple solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+	if (number >= 1 && number <= Math.pow(10,8)) {
+  		var result;
+  		var index = 5;
+  		var sumResult = 0;
+  		do {
+  			result = (number / index) - ((number / index) % 1);
+  			index = index * 5;
+  			sumResult += result;
+  		} while(result > 1)
+	}
+
+	return sumResult;		
 }
+
